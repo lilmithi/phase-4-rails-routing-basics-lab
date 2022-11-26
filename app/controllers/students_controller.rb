315c96c5e95bd2cdbd17571c3425900396681006
martin mithi <gathitumithi9000@gmail.com>
2022-11-26 11:48:09 +0300

    commit tree 633791d7c3ebfd16aae41ce0111de1e17799df4e

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+    def index
+        students=Student.all
+        render json: students
+    end
+
+    def grades
+        students=Student.all
+        render json: students.order(grade: :desc)
+    end
+end
